3bf5977e8120c8807a64a02e914a4502c7bc519a
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-08-26 18:26:49 +0300

    add command for making "BUILD_ID to AOSP tag" map

---
+++ src/util/git.ts
@@ -0,0 +1,39 @@
+import assert from 'assert'
+import { spawnAsync } from './process'
+
+export class GitLsRemote {
+  tags = new Map<string, string>()
+  branches = new Map<string, string>()
+
+  static async get(repoUrl: string) {
+    let lsRemote = await spawnAsync('git', ['ls-remote', '--tags', '--heads', repoUrl])
+
+    let result = new GitLsRemote()
+
+    const tagPrefix = 'refs/tags/'
+    const tagSuffix = '^{}'
+    const branchPrefix = 'refs/heads/'
+
+    for (let line of (await lsRemote).split('\n')) {
+      if (line.length === 0) {
+        continue
+      }
+      let split = line.split('\t')
+      assert(split.length == 2, line)
+      let commit = split[0]
+      let desc = split[1]
+
+      if (desc.startsWith(tagPrefix)) {
+        if (!desc.endsWith(tagSuffix)) {
+          continue
+        }
+        result.tags.set(desc.slice(tagPrefix.length, - tagSuffix.length), commit)
+      } else {
+        assert(desc.startsWith(branchPrefix), line)
+        result.branches.set(desc, commit)
+      }
+    }
+
+    return result
+  }
+}
