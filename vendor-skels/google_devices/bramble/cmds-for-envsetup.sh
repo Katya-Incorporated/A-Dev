6d0d318cd0bba7504b9410711cdaf57f5857eeb8
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-02-12 11:58:12 +0200

    switch bramble and redfin to UP1A.231105.001.B2

--- vendor-skels/google_devices/bramble/cmds-for-envsetup.sh
+++ vendor-skels/google_devices/bramble/cmds-for-envsetup.sh
@@ -1,2 +1,2 @@
-export BUILD_ID_bramble="UP1A.231105.001"
+export BUILD_ID_bramble="UP1A.231105.001.B2"
 export PLATFORM_SECURITY_PATCH_bramble="2023-12-01"
--- vendor-skels/google_devices/redfin/cmds-for-envsetup.sh
+++ vendor-skels/google_devices/redfin/cmds-for-envsetup.sh
@@ -1,2 +1,2 @@
-export BUILD_ID_redfin="UP1A.231105.001"
+export BUILD_ID_redfin="UP1A.231105.001.B2"
 export PLATFORM_SECURITY_PATCH_redfin="2023-12-01"
